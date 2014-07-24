8ed694a081dfe882ef09d3479a30a5a82f443b45
Daniel Bogart <daniel.e.bogart@gmail.com>
2014-07-24 12:03:26 -0700

    Added relations

--- js/app.js
+++ js/app.js
@@ -1,24 +1,66 @@
 $(document).ready(function(){
 
+	var cleaningSupplies = [
+		{id: 'Paper Towels', site: '#'},
+		{id: 'Mop', site: '#'},
+		{id: 'Broom', site: '#'},
+		{id: 'Swiffer', site: '#'}
+	];
+
+	var kitchenSupplies = [
+		{id: 'Pans', site: '#'},
+		{id: 'Spatula', site: '#'},
+		{id: 'Glasses', site: '#'},
+		{id: 'Plates', site: '#'}
+	];
+
+	var furniture = [
+		{id: 'Couch', site: '#'},
+		{id: 'Coffee Table', site: '#'},
+		{id: 'Dining Table', site: '#'},
+		{id: 'Dining Chairs', site: '#'}
+	];
+
+	var bedroomBasics = [
+		{id: 'Bed', site: '#'},
+		{id: 'Night Stand', site: '#'},
+		{id: 'Dresser', site: '#'},
+		{id: 'Desk', site: '#'}
+	];
+
+	var generalFood = [
+		{id: 'Olive Oil', site: '#'},
+		{id: 'Salt', site: '#'},
+		{id: 'Pepper', site: '#'},
+		{id: 'Sriracha', site: '#'}
+	];
+
+	var randomStuff = [
+		{id: 'Batteries', site: '#'},
+		{id: 'Flashlight', site: '#'},
+		{id: 'First Aid Kit', site: '#'},
+		{id: 'Basic Toolkit', site: '#'}
+	];
+
+	var buildList = function (categoryName, categoryTitle) {
+
 		var list = document.createElement('div');
 		var listCat = document.createElement('a');
 		var listItem = document.createElement('a');
 		var listCheckBox = document.createElement('div');
 		var listLabel = document.createElement('label');
 		var listInput = document.createElement('input');
-		var catTitle = "Example Title";
-		var itemText = "Example item";
+		var catTitle = categoryTitle;
 
-		list.className = 'list-group col-xs-10 col-xs-offset-1 col-md-6 col-md-offset-3';
+		list.className = 'list-group col-xs-10 col-xs-offset-1 col-md-6 col-md-offset-3 scooby';
 		listCat.className = 'list-group-item active cat';
 		listItem.className = 'list-group-item carrot';
 		listCheckBox.className = 'checkbox smallMargin';		
 		
 		listCat.innerHTML = catTitle;
-		listLabel.innerHTML = itemText;
+		listLabel.innerHTML = categoryName[0].id;
 
-	var buildList = function () {
-
+		if ($('.list-group').length == 0) {
 		$('#heresWhat').after(list);
 		$('.list-group').append(listCat);
 				
@@ -27,8 +69,23 @@ $(document).ready(function(){
 		$('.checkbox').append(listLabel);
 		$('label').prepend(listInput);
 		$('label').children().attr('type', 'checkbox');
+
+		}	
+
+		else {
+
+		$('.scooby').last().after(list);
+		$('.scooby').last().append(listCat);
+				
+		$('.cat').last().after(listItem);
+		$('.carrot').last().append(listCheckBox);
+		$('.checkbox').last().append(listLabel);
+		$('label').last().prepend(listInput);
+		$('label').last().children().attr('type', 'checkbox');
+
+		};
 		
-		for (var x = 0; x <5; x++) {
+		for (var x = 1, y = categoryName.length; x < y; x++) {
 			var listItem2 = document.createElement('a');
 			var listCheckBox2 = document.createElement('div');
 			var listLabel2 = document.createElement('label');
@@ -40,7 +97,7 @@ $(document).ready(function(){
 			listItem2.className = 'list-group-item carrot';
 			listCheckBox2.className = 'checkbox smallMargin';		
 			
-			listLabel2.innerHTML = itemText;
+			listLabel2.innerHTML = categoryName[x].id;
 
 			$('.carrot:last').after(listItem2);
 			$('.carrot:last').append(listCheckBox2);
@@ -62,13 +119,35 @@ $('#soWhat').click(function() {
 	$('#page1').hide();
 	$('#page2').show();
 
-	buildList();
+	if ($('#cSupplies').hasClass('active')) {
+		buildList(cleaningSupplies, 'Cleaning Supplies'	);
+	};
+
+	if ($('#kSupplies').hasClass('active')) {
+		buildList(kitchenSupplies, 'Kitchen Supplies');
+	};
+
+	if ($('#furniture').hasClass('active')) {
+		buildList(furniture, 'Furniture');
+	};
+
+	if ($('#bedroomBasics').hasClass('active')) {
+		buildList(bedroomBasics, 'Bedroom Basics');
+	};
+
+	if ($('#generalFood').hasClass('active')) {
+		buildList(generalFood, 'General Food');
+	};
+
+	if ($('#randomStuff').hasClass('active')) {
+		buildList(randomStuff, 'Random Stuff');
+	};
 
 });
 
 $('#back').click(function() {
 
-	$('.list-group').children().remove();
+	$('.list-group').remove();
 	$('#page2').hide();
 	$('#page1').show();
 
